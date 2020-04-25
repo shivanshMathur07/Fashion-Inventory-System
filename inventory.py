25e29cf0b373bbca7a10af2be669e17f412f4ee7
Shivansh <shivanshmathurr@gmail.com>
2020-04-25 14:47:29 +0530

    commit-2

--- inventory.py
+++ inventory.py
@@ -2,19 +2,30 @@ import sqlite3
 
 # defining connection
 db = sqlite3.connect('custom.db')
-
+# to add database
 def writeDataCustomer(name , cat , color , price , pay_r , pay_l , contact):
     db.execute(''' INSERT INTO customers(Name,Category,Colour,Price,'Payment Received','Payment Left',Contact)
         VALUES(?,?,?,?,?,?,?)''',(name , cat , color , price , pay_r , pay_l , contact))
 
     db.commit()
+# to read database
 def readDataCustomer():
     return(db.execute(''' SELECT * FROM customers '''))
+
+# merchant database
+def writeDataMerchant(name ,order , price , colour , size , shipping ,total_price, total_paid, total_left):
+    db.execute(''' INSERT INTO merchant(Name,'Order Name','Merchant Price',Colour,Size,Shipping,'Total Price','Total Paid','Total Left')
+        VALUES(?,?,?,?,?,?,?,?,?)''',(name,order,price, colour , size , shipping ,total_price, total_paid,total_left))
+
+    db.commit()
+
+def readDataMerchant():
+    return(db.execute(''' SELECT * FROM merchant '''))
 
 # taking input entries
 run = True
 while(run):
-    print('''(1).Add new order entry\n(2)Add new merchant entry\n(3).To check profit\n(4)Add a note\n(5)View all customer records\n(6)Exit''')
+    print('''(1).Add new order entry\n(2)Add new merchant entry\n(3).To check profit\n(4)Add a note\n(5)View all customer records\n(6)View merchant records\n(7)Exit''')
     ch = int(input('Enter your choice :'))
     # to add new order
     if ch == 1:
@@ -28,7 +39,17 @@ while(run):
         writeDataCustomer(name,order_cat,order_col,order_price,pay_r,pay_left,contact)
     # to add new merchant entry
     elif ch == 2 :
-        pass
+        name = input('Enter the name of merchant :')
+        orderName = input('Enter the name of order :')
+        mprice = int(input('Enter the price of order from merchant :'))
+        color = input('Enter the colour of order :')
+        size = (input('Size of order :'))
+        shipping = int(input("Enter the shipping charges :"))
+        tprice = mprice + shipping
+        print(f'Total price is :{tprice}')
+        tpaid =int(input("Enter total amount paid :"))
+        tleft = tprice - tpaid
+        writeDataMerchant(name,orderName,mprice,color,size,shipping,tprice,tpaid,tleft)
     elif ch == 3 :
         pass
     elif ch == 4 :
@@ -37,7 +58,11 @@ while(run):
         data = readDataCustomer()
         for entry in data:
             print(f'Id: {entry[0]}\nName: {entry[1]}\nCategory: {entry[2]}\nColor: {entry[3]}\nPrice: {entry[4]}\nPay Received: {entry[5]}\nPay Left: {entry[6]}\nContact: {entry[7]}\n\n')          
-    elif ch == 6 :
+    elif ch == 6:
+        data = readDataMerchant()
+        for entry in data:
+            print(f'Id: {entry[0]}\nName: {entry[1]}\nOrder Name: {entry[2]}\nMerchant Price: {entry[3]}\nColour: {entry[4]}\nSize: {entry[5]}\nShipping: {entry[6]}\nTotal Price: {entry[7]}\nTotal Paid: {entry[8]}\nTotal Left: {entry[9]}\n\n')
+    elif ch == 7 :
         run = False
         db.close()
     else:
