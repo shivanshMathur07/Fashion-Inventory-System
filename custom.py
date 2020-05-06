420cbf4f80808880c78e31fd95a336bd0552f5a0
Shivansh <shivanshmathurr@gmail.com>
2020-05-06 22:42:05 +0530

    final commit

--- custom.py
+++ custom.py
@@ -1,6 +1,39 @@
 import sqlite3
 
-dbc = sqlite3.connect('custom.db')
+def newDB():
+    dbc = sqlite3.connect('custom.db')
+    query=''' CREATE TABLE customers (
+    [C-Id]             INTEGER      PRIMARY KEY AUTOINCREMENT,
+    Name               CHAR (30)    NOT NULL,
+    Category           CHAR (30)    NOT NULL,
+    Colour             CHAR (20)    NOT NULL,
+    Size               VARCHAR (10) NOT NULL
+                                    DEFAULT None,
+    Price              INT (10)     NOT NULL,
+    [Payment Received] INT (10),
+    [Payment Left]     INT (10),
+    Contact            NUMERIC (11) 
+    ) '''
+
+    dbc.execute(query)
+
+    query = ''' CREATE TABLE Merchant (
+    Id               INTEGER      PRIMARY KEY AUTOINCREMENT,
+    Name             CHAR (25)    NOT NULL,
+    [Order Name]     CHAR (30)    NOT NULL,
+    [Merchant Price] INTEGER (10) NOT NULL,
+    Colour           CHAR (20)    NOT NULL,
+    Size             VARCHAR (10) NOT NULL,
+    Shipping         INTEGER (10),
+    [Total Price]    INTEGER (10) NOT NULL
+                                  DEFAULT (0),
+    [Total Paid]     INTEGER (10) DEFAULT (0),
+    [Total Left]     INTEGER (10),
+    [Order For]      CHAR (30)    DEFAULT [No Name]
+                                  NOT NULL
+)'''
+
+    dbc.execute(query)
 
 def writeDataCustomer(nameEnt, cateEnt, colorEnt, sizeEnt, tpriceEnt, payrEnt, contactEnt, MNameEnt, MpriceEnt, shipEnt, TlpaidEnt, ):
     dbc = sqlite3.connect('custom.db')
@@ -46,6 +79,7 @@ def readDataMerchant():
     return(dbc.execute(''' SELECT * FROM merchant '''))
 
 def checkProfit():
+    dbc = sqlite3.connect('custom.db')
     totalCost = 0
     totalPayR = 0
     totalPayL = 0
@@ -59,5 +93,6 @@ def checkProfit():
         totalPayL += d[1]
     actualProfit = (totalPayR+totalPayL) - totalCost
     currentProfit = totalPayR - totalCost
+    dbc.close()
     return(actualProfit,currentProfit,totalPayL)
 
